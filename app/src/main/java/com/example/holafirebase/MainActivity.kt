a8051c9fc6430ba82f95cd0b89b47c1fbf611ec3
Josue <sjosue5082002@gmail.com>
2022-01-11 17:51:16 -0600

    Leer datos remotos en tiempo real

--- app/src/main/java/com/example/holafirebase/MainActivity.kt
+++ app/src/main/java/com/example/holafirebase/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.holafirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = Firebase.database.reference //acceder a la reiz de la base de datos
+
+        val listener = object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) { //cuando hay un cambio hace el evento
+                val data = snapshot.getValue(String::class.java)
+                findViewById<TextView>(R.id.tvData).text = "Firebase remote: $data"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        val dataRef = database.child("hola_firebase")//nombre del hijo de la raiz (rama)
+            .child("data")//el hijo de la rama
+
+        dataRef.addValueEventListener(listener)
     }
 }
